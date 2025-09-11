8b0c4f46a146f9767027ea59149be8723150efed
missviee <viesbusiness@gmail.com>
2025-09-11 23:44:51 +0300

    Save progress

---
+++ terraform/ecr.tf
@@ -0,0 +1,7 @@
+resource "aws_ecr_repository" "statuspage" {
+  name = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration { scan_on_push = true }
+}
+
+output "ecr_repo_url" { value = aws_ecr_repository.statuspage.repository_url }
