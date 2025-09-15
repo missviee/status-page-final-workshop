b8f6554f21f7be2c22b4132861e28a29ac37af60
Rachel Yakobov <rachelyakobov28@gmail.com>
2025-09-15 16:27:31 +0300

    Upload files

--- terraform/eks_step1.tf
+++ terraform/eks_step1.tf
@@ -8,6 +8,10 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access_cidrs = ["79.181.174.149/32"]
+
   eks_managed_node_groups = {
     dr_nodes = {
       desired_size   = 2
@@ -17,4 +21,14 @@ module "eks" {
     }
   }
 }
+
+resource "aws_security_group_rule" "eks_api_access" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["79.181.174.149/32"]
+  security_group_id = module.eks.cluster_security_group_id
+}
+
 
